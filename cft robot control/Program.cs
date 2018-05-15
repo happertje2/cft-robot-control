b931f6c142448c7b3895c288cc121d58e8c35151
jelle <jelleschafelt@hotmail.com>
2018-05-15 21:21:57 +0200

    cant open the communication to the roboclaw controllers

--- cft robot control/Program.cs
+++ cft robot control/Program.cs
@@ -4,20 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using RoboclawClassLib;
+
 namespace cft_robot_control
 {
-    static class Program
+    public static partial class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-            //here we go!
+			string roboClawModel = "emty";
+            int baudRate = 38400;
+            byte roboClawAdress = 128;
+
+			Roboclaw roboclaw;
+			roboclaw = new Roboclaw("AUTO", baudRate, roboClawAdress);
+			roboclaw.Open();
+
+			//roboclaw = new Roboclaw();
+			//roboclaw.Open("AUTO", ref roboClawModel,roboClawAdress, baudRate);
+
+			Console.WriteLine(roboclaw.IsOpen());
         }
     }
 }
